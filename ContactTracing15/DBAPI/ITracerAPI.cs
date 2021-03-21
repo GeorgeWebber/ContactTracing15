5f2c31faaba1a4a7c7d49deb5db053993eae353f
CosmicRay11 <george.w11@hotmail.co.uk>
2021-03-21 15:05:43 +0000

    Added new methods to the tracer API interface to specify adding new contacts, assigning new cases and editing contact data

--- ContactTracing15/DBAPI/ITracerAPI.cs
+++ ContactTracing15/DBAPI/ITracerAPI.cs
@@ -17,5 +17,27 @@ namespace ContactTracing15.DBAPI
         /// </summary>
         /// <returns>A Task object containing a list of ** type </returns>
         public Task GetActiveCases(int testerID);
+
+        // TODO : edit return method to specify model type
+        /// <summary>
+        /// Assign a new case to the contact tracer with a given ID
+        /// </summary>
+        /// <returns>A Task object containing a model containing the new case's data </returns>
+        public Task AssignNewCase(int testerID);
+
+        // TODO : edit parameter to specify model type
+        /// <summary>
+        /// Add a new contact to the contact table of the database
+        /// </summary>
+        /// <returns>True if data is successfully added, False otherwise.</returns>
+        public Task<Boolean> AddNewContact();
+
+        // TODO : edit parameter to specify model type
+        /// <summary>
+        /// Edit a contact's data in the database
+        /// </summary>
+        /// <returns>Task object with True if data successfully modified, False otherwise. </returns>
+        public Task<Boolean> EditContact();
+
     }
 }
