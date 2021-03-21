d857000759c6fa88587edded3078fd4bbae22dc1
CosmicRay11 <george.w11@hotmail.co.uk>
2021-03-21 14:58:08 +0000

    Add new folders to store models ("Models") and database api classes ("DBAPI"). Add interfaces to specify the commands available to testers and contact tracers

---
+++ ContactTracing15/DBAPI/ITesterAPI.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace ContactTracing15.DBAPI
+{
+    /// <summary>
+    /// An interface to specify what commands should be available to the testing facility users
+    /// </summary>
+    interface ITesterAPI
+    {
+
+        /// <summary>
+        /// Add data on a positive case to the database
+        /// *Should take a parameter - this parameter will be a model type I believe - George*
+        /// </summary>
+        /// <returns>Task object specifying the result of the operation</returns>
+        public Task AddCase();
+    }
+
+     
+}
