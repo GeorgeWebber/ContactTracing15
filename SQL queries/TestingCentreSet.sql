b99bda77d50645e88a9dfcca8eb6bc1020cc8a74
Dmitry Filippov <dmitriyvfilippov@gmail.com>
2021-04-09 19:25:46 +0100

    Wrote SQL queries to populate database. Supposedly created a migration (If it doesn't populate database, just run the queries on your machine). Also added DP project for no apparent reason other than why not.

---
+++ SQL queries/TestingCentreSet.sql
@@ -0,0 +1,10 @@
+﻿Declare @Id int
+Set @Id = 1
+
+While @Id <= 100
+Begin 
+   Insert Into TestingCentres values ('Testing Centre - ' + CAST(@Id as nvarchar(10)),
+              'Postcode - ' + CAST(@Id as nvarchar(10)))
+   Print @Id
+   Set @Id = @Id + 1
+End
---
+++ TracingAppDB/dbo/Tables/Tracers.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Tracers] (
+    [TracerID]        INT            IDENTITY (1, 1) NOT NULL,
+    [Username]        NVARCHAR (MAX) NULL,
+    [TracingCentreID] INT            NOT NULL,
+    CONSTRAINT [PK_Tracers] PRIMARY KEY CLUSTERED ([TracerID] ASC),
+    CONSTRAINT [FK_Tracers_TracingCentres_TracingCentreID] FOREIGN KEY ([TracingCentreID]) REFERENCES [dbo].[TracingCentres] ([TracingCentreID]) ON DELETE CASCADE
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_Tracers_TracingCentreID]
+    ON [dbo].[Tracers]([TracingCentreID] ASC);
+
