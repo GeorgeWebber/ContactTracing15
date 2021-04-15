2564c78c9178529032853970f8c1f7c06c05dba6
CosmicRay11 <george.w11@hotmail.co.uk>
2021-04-15 14:54:35 +0100

    Tidied up method of geolocating postcodes, and implemented error catching

--- ContactTracing15/Pages/GovAgent/GovHome.cshtml.cs
+++ ContactTracing15/Pages/GovAgent/GovHome.cshtml.cs
@@ -26,40 +26,62 @@ namespace ContactTracing15.Pages.GovAgent
 
         public void OnGet()
         {
-            var Postcodes = new string[] {"OX1", "OX16"};
+            var Postcodes = new string[] {"OX1", "OX16", "OX1", "OX2", "OX3", "OX4", "OX5", "OX14", "SS11"};
 
-            Lats = new double[Postcodes.Count()];
-            Longs = new double[Postcodes.Count()];
+            double[] latArray = new double[Postcodes.Count()];
+            double[] longArray = new double[Postcodes.Count()];
+
+            
 
             int i = 0;
             foreach (string postcode in Postcodes)
             {
                 Tuple<double, double> location = GetLocation(postcode);
-                Lats[i] = location.Item1;
-                Longs[i] = location.Item2;
-                i++;
+                Console.WriteLine(location);
+                if (location != null)
+                {
+                    latArray[i] = location.Item1;
+                    longArray[i] = location.Item2;
+                    i++;
+                }
             }
+            Lats = new double[i];
+            Longs = new double[i];
+
+            Array.Copy(latArray, Lats, i);
+            Array.Copy(longArray, Longs, i);
+
 
         }
 
         public Tuple<double, double> GetLocation(string postcode)
         {
-            string requestUri = string.Format("https://maps.googleapis.com/maps/api/geocode/xml?key={1}&address={0}&sensor=false", Uri.EscapeDataString(postcode), _config["googleApiKey"]);
+            try
+            {
+                string requestUri = string.Format("https://maps.googleapis.com/maps/api/geocode/xml?key={1}&components=country:GB|postal_code:{0}&sensor=false", Uri.EscapeDataString(postcode), _config["googleApiKey"]);
 
-            Console.WriteLine(_config["googleApiKey"]);
+                Console.WriteLine(_config["googleApiKey"]);
 
-            WebRequest request = WebRequest.Create(requestUri);
-            WebResponse response = request.GetResponse();
-            XDocument xdoc = XDocument.Load(response.GetResponseStream());
+                WebRequest request = WebRequest.Create(requestUri);
+                WebResponse response = request.GetResponse();
+                XDocument xdoc = XDocument.Load(response.GetResponseStream());
 
-            XElement result = xdoc.Element("GeocodeResponse").Element("result");
-            XElement locationElement = result.Element("geometry").Element("location");
-            XElement lat = locationElement.Element("lat");
-            XElement lng = locationElement.Element("lng");
+                XElement result = xdoc.Element("GeocodeResponse").Element("result");
+                XElement locationElement = result.Element("geometry").Element("location");
+                XElement lat = locationElement.Element("lat");
+                XElement lng = locationElement.Element("lng");
 
-            Console.WriteLine(lat);
+                Console.WriteLine(lat);
 
-            return new Tuple<double, double>((double) lat, (double) lng);
+                return new Tuple<double, double>((double)lat, (double)lng);
+            }
+            catch {
+                return null;
+            };
+            
+            
+
+
 
         }
 
