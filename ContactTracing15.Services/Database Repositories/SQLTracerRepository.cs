2d27461c6cba53b2b925720d0c3d52cf82d9968a
Dmitry Filippov <dmitriyvfilippov@gmail.com>
2021-04-25 13:37:59 +0100

    GetTracerWithLeastCases returns 2 tracers

--- ContactTracing15.Services/Database Repositories/SQLTracerRepository.cs
+++ ContactTracing15.Services/Database Repositories/SQLTracerRepository.cs
@@ -59,18 +59,17 @@ namespace ContactTracing15.Services
               .FirstOrDefault();
         }
         
-        public Tracer GetTracerWithLeastCases()
+        public IEnumerable<Tracer> GetTracerWithLeastCases()
         {
             return context.Tracers
               .FromSqlRaw<Tracer>(@"select * from tracers where  TracerID = (
-                                    select top (1) t.tracerid
+                                    select top (2) t.tracerid
                                     from tracers t 
                                     left join (select * from cases where (Traced = 0)) c
                                     on c.tracerid = t.tracerID
                                     group by t.tracerid
                                     order by count(c.tracerid) asc)")
-              .ToList()
-              .FirstOrDefault();
+              .ToList();
         }
         
         public IEnumerable<Tracer> Search(string searchTerm)
