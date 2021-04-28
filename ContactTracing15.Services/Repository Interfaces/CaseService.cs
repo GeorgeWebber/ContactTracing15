b9da551500ac1565dc4f254cde3627c4e891035c
Dmitry Filippov <dmitriyvfilippov@gmail.com>
2021-04-28 12:44:38 +0100

    made average/percentage methods work when 0 cases

--- ContactTracing15.Services/Repository Interfaces/CaseService.cs
+++ ContactTracing15.Services/Repository Interfaces/CaseService.cs
@@ -91,6 +91,7 @@ namespace ContactTracing15.Services
         double ICaseService.PercentageCasesReachedLast28Days()
         {
             int cases = _caseRepository.GetCasesByDate(DateTime.Now, DateTime.Now.AddDays(-28)).ToList().Count();
+            if (cases == 0) { return 0; }
             int traced = _caseRepository.GetCasesByDate(DateTime.Now, DateTime.Now.AddDays(-28)).Where(x => x.Traced).ToList().Count();
             return traced / cases * 100;
         }
