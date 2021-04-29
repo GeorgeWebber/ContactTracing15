99447f946d6d13bb2c699489e4790a2d81c8afe3
CosmicRay11 <george.w11@hotmail.co.uk>
2021-04-29 12:40:48 +0100

    Secure password in secret vault for email

--- ContactTracing15.Services/EmailService.cs
+++ ContactTracing15.Services/EmailService.cs
@@ -8,22 +8,31 @@ using System.Net.Mail;
 using System.Net.Mime;
 using System.Threading;
 using System.ComponentModel;
+using Microsoft.Extensions.Configuration;
 
 namespace ContactTracing15.Services
 {
     public class EmailService : IEmailService
     {
+
+        IConfiguration _config;
+
+        public EmailService (IConfiguration config)
+        {
+            _config = config;
+        }
+
         public void ContactByEmail(Contact contact)
         {
             //TODO send email to contact
             SmtpClient smtpClient = new SmtpClient("smtp.gmail.com", 587);
             smtpClient.UseDefaultCredentials = false;
-            smtpClient.Credentials = new System.Net.NetworkCredential("x23730389@gmail.com", "1380slfF");
+            smtpClient.Credentials = new System.Net.NetworkCredential("groupdesignproject15@gmail.com", _config["emailPassword"]);
             smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
             smtpClient.EnableSsl = true;
             MailMessage mail = new MailMessage();
             //Setting From , To and CC
-            mail.From = new MailAddress("x23730389@gmail.com");
+            mail.From = new MailAddress("groupdesignproject15@gmail.com");
             mail.Subject = "YOU GOT A FREND WITH COVID";
             mail.Body = "HAHA BRO ITS JUST A TEST";
             mail.To.Add(new MailAddress(contact.Email));
