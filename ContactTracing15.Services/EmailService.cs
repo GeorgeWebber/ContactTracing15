0d0aaaf13b0134636ff4d990a14ccefce6be12ee
Lanceward <lianfengshi@outlook.com>
2021-04-29 11:01:57 +0100

    adding email function

--- ContactTracing15.Services/EmailService.cs
+++ ContactTracing15.Services/EmailService.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using System.Text;
 using ContactTracing15.Models;
 using ContactTracing15.Services;
+using System.Net;
+using System.Net.Mail;
+using System.Net.Mime;
+using System.Threading;
+using System.ComponentModel;
 
 namespace ContactTracing15.Services
 {
@@ -11,6 +16,15 @@ namespace ContactTracing15.Services
         public void ContactByEmail(Contact contact)
         {
             //TODO send email to contact
+            SmtpClient smtpClient = new SmtpClient("smtp.gmail.com", 25);
+            smtpClient.UseDefaultCredentials = true;
+            smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
+            smtpClient.EnableSsl = true;
+            MailMessage mail = new MailMessage();
+            //Setting From , To and CC
+            mail.From = new MailAddress("x23730389@gmail.com", "Testing Email");
+            mail.To.Add(new MailAddress(contact.Email));
+            smtpClient.Send(mail);
         }
     }
 }
