f8cf19ea36c7fe71040a4328618fa2063cf71c0d
Lanceward <lianfengshi@outlook.com>
2021-05-01 19:37:15 +0100

    Adding scheduled tasks

--- ContactTracing15.Services/IPrivacyService.cs
+++ ContactTracing15.Services/IPrivacyService.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿using Microsoft.Extensions.Hosting;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace ContactTracing15.Services
 {
-    public interface IPrivacyService
+    public interface IPrivacyService: IHostedService, IDisposable
     {
         public void CleanOldRecords();
     }
